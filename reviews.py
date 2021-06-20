7768043238f20735fb76f32adad261937f823221
yao-ying228 <wowowo228@msn.com>
2021-06-20 19:01:10 +0800

    有基本篩選的程式碼

--- reviews.py
+++ reviews.py
@@ -4,7 +4,7 @@ with open('reviews.txt','r') as f :
 	for line in f :
 		data.append(line)
 		count +=1
-		if count % 1000 == 0 :
+		if count % 50000 == 0 :
 		    print(len(data))
 print('檔案讀取完畢總共有' , len(data) , '筆資料')
 
@@ -15,4 +15,12 @@ for d in data:
 print('平均留言長度為:',sum_len/len(data),'個字數')	
 
 
+new = []
+for d in data:
+	if len(d) < 100:
+	   new.append(d)
+print('總共有' , len(new) , '筆留言字數<100個字')
 
+print(new[0])
+print('---------')
+print(new[239])
